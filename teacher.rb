d566d8d0801abc0051d39237a7d6fac62ada6945
shaheryar abid <sharyar0310@gmail.com>
2023-07-25 18:04:55 +0500

    Added three diffrents files base

---
+++ teacher.rb
@@ -0,0 +1,15 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+teacher = Teacher.new(30, 'Mathematics', 'Jack')
+puts teacher.can_use_services?
