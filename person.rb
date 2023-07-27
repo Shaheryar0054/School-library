a8dcfade48392b2d630d6ecfb57e5563c0f590b2
shaheryar abid <sharyar0310@gmail.com>
2023-07-27 16:24:27 +0500

    Implemented the required changes

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
-require './nameable'
-require './trimmer_decorator'
-require './capitalize_decorator'
+require_relative './nameable'
+require_relative './trimmer_decorator'
+require_relative './capitalize_decorator'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
