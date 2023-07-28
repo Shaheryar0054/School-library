03db25f51a7ec1694d05ed5b11aecb4afd89f8ec
shaheryar abid <sharyar0310@gmail.com>
2023-07-28 22:47:14 +0500

    Implemented the suggested changes

--- person.rb
+++ person.rb
@@ -9,9 +9,14 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
+
+  def add_rental(rental)
+    @rentals << rental
+  end
 
   def correct_name
     @name
@@ -27,3 +32,14 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+# See the decorators in action
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
